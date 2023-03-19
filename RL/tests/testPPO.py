3b644fdc8325f81ea30a11fea1c943fb40ce5155
Zeyad Alaa <zeyad.alaa171@gmail.com>
2023-03-19 16:56:18 +0200

    Added README

--- RL/tests/testPPO.py
+++ RL/tests/testPPO.py
@@ -5,6 +5,7 @@ import gym
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
+import replay
 
 models_dir = "../models/"
 env_kwargs = {'config': "hangzhou_1x1_bc-tyc_18041607_1h", 'steps_per_episode': 100, 'steps_per_action': 30}
@@ -13,9 +14,9 @@ env_kwargs = {'config': "hangzhou_1x1_bc-tyc_18041607_1h", 'steps_per_episode': 
 def train():
     env = make_vec_env('CityFlowRL-v0', n_envs=12, vec_env_cls=SubprocVecEnv, env_kwargs=env_kwargs)
 
-    model = PPO('MlpPolicy', env, verbose=2, tensorboard_log="./tensorboard/", batch_size=32, n_steps=24)
+    model = PPO('MlpPolicy', env, verbose=2, tensorboard_log="../tensorboard/", batch_size=32, n_steps=24)
     # model = PPO.load("ppo_3", env=env)
-    model.learn(total_timesteps=3000, reset_num_timesteps=False)
+    model.learn(total_timesteps=100000, reset_num_timesteps=False)
     model.save(os.path.join(models_dir, "ppo"))
     print("model saved")
 
@@ -26,7 +27,7 @@ def test():
     env = DummyVecEnv([lambda: env])
 
     model = PPO.load("ppo")
-    episodes = 10
+    episodes = 1
     for ep in range(episodes):
         obs = env.reset()
         done = False
@@ -35,6 +36,8 @@ def test():
             obs, reward, done, info = env.step(action)
         print(info)
     env.close()
+    replay.run(env_kwargs['config'])
+
 
 
 if __name__ == "__main__":
